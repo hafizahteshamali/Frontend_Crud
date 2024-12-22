f5b34fd1b61e30ced8fea2790d29553fa695b6d9
hafizahteshamali <hafizahteshamali363617@gmail.com>
2024-12-22 17:58:06 +0500

    all frontend code

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://mongo-db-crud-flame.vercel.app",
+  baseURL: "https://backend-curd-blond.vercel.app/",
   timeout: 2000,
   headers: {
     "Content-Type": "application/json",
